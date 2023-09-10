54509fbb4addcefe75b14ae8e6e8f56c541fa89f
melina-niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-09-10 22:10:03 +0330

    checking days

--- session3.ts
+++ session3.ts
@@ -27,28 +27,43 @@ if ( grade >= 90 ) {
 };
 
 
-// check the light
+// check the traffic light
 
-// let currentColor:string = "red"
-let currentColor:string = prompt("enter color:").toLowerCase()
+let currentColor:string = "red"
+// let currentColor:string = prompt("enter color:").toLowerCase()
 
-// if ( currentColor === "red" ) {
-//     console.log("The next color is green!")
-// } else if ( currentColor === "yellow" ) {
-//     console.log("The next color is red!")
-// } else if ( currentColor === "green" ) {
-//     console.log("The next color is yellow!")
-// }
-
-switch (currentColor) {
-    case "red" :
-        console.log("The next color is green!")
-        break;
-    case "yellow" :
-        console.log("The next color is red!")
-        break;
-    case "red" :
-        console.log("The next color is yellow!")
-        break; 
+if ( currentColor === "red" ) {
+    console.log("The next color is green!")
+} else if ( currentColor === "yellow" ) {
+    console.log("The next color is red!")
+} else if ( currentColor === "green" ) {
+    console.log("The next color is yellow!")
 }
 
+//  Days
+
+let currentDay:number = 5
+
+switch (currentDay) {
+    case 1 :
+        console.log("Saturday")
+        break;
+    case 2 :
+        console.log("Sunday")
+        break;
+    case 3 :
+        console.log("Monday")
+        break; 
+    case 4 :
+        console.log("Tuesday")
+        break; 
+    case 5 :
+        console.log("Wednesday")
+        break; 
+    case 6 :
+        console.log("Thursday")
+        break; 
+    case 7 :
+        console.log("Friday")
+        break; 
+}
