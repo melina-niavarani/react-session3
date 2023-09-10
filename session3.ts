6a6919f0aa88046a7b300b5c2a0c18070b5fba82
melina-niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-09-10 21:38:51 +0330

    check your grade

--- session3.ts
+++ session3.ts
@@ -2,3 +2,22 @@ let requiredAge: number = 18 ;
 let userAge: number = 15 ;
 
 userAge > requiredAge ? console.log("Welcome!") :  console.log("You are not allowed.")
+
+
+// calculate your score
+
+let grade: number = 69
+
+if ( grade >= 90 ) {
+    console.log ("A")
+}else if ( grade >= 80 ) {
+    console.log ("B")
+}else if ( grade >= 70 ) {
+    console.log ("C")
+}else if ( grade >= 60 ) {
+    console.log ("D")
+}else if ( grade >= 50 ) {
+    console.log ("E")
+}else  {
+    console.log ("F")
+};
